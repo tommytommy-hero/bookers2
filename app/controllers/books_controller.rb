d932b964789f84021ca1052c95b18b983f3938fd
tommytommy-ヒーロー <109708488+tommytommy-hero@users.noreply.github.com>
2022-08-10 04:22:28 +0000

    [finish]課題３　Bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,70 @@
+class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
+  def new
+    @book=Book.new
+  end
+
+  def create
+    @books=Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user=current_user
+    if @book.save
+      flash[:notice]="You have created successfully."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
+  end
+
+  def index
+    @books=Book.all
+    @book=Book.new
+    @user=current_user
+  end
+
+  def show
+    @book=Book.find(params[:id])
+    @books=Book.all
+    @user=@book.user
+    @newbook=Book.new
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success]="You have updates book successfully."
+      redirect_to book_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
+end
